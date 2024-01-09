b0890e5593b7605c721918104fbb92f3600c781d
Sahil Kumar <sahil23012000@gmail.com>
2024-01-09 22:46:35 +0530

    edit: updated Error Page

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React from "react";
 import "./error.css";
+import plugImage from "../../assets/rotated-plug.png";
+import CrossImage from "../../assets/cross.svg";
+import CircleImage from "../../assets/circle.svg";
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
-  )
-}
+    <main className="error-page">
+      <section className="wire-section">
+        <img src={plugImage} alt="Plug" />
+      </section>
 
-export default Error
+      <section className="error-text-section">
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>
+          we’re sorry. the page you requested could no be found Please go back
+          to the home page
+        </p>
+        <button>Go Home</button>
+
+        <img className="cross-img" image-size="small" src={CrossImage} alt="" />
+        <img
+          className="cross-img"
+          image-size="medium"
+          src={CrossImage}
+          alt=""
+        />
+        <img
+          className="cross-img"
+          image-size="large"
+          src={CrossImage}
+          alt=""
+        />
+
+        <img
+          className="circle-img"
+          image-size="small"
+          src={CircleImage}
+          alt=""
+        />
+        <img
+          className="circle-img"
+          image-size="medium"
+          src={CircleImage}
+          alt=""
+        />
+        <img
+          className="circle-img"
+          image-size="large"
+          src={CircleImage}
+          alt=""
+        />
+      </section>
+    </main>
+  );
+};
+
+export default Error;
